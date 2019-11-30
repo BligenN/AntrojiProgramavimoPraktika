793b3fd7e614cefe5078f32d532d2bcbd66df6a7
Paulius Martišius <paulius.martisius@tesonet.com>
2019-11-30 14:04:48 +0200

    First commit

---
+++ app/src/main/java/com/example/antrojiprogramavimopraktika/login/LoginFragment.kt
@@ -0,0 +1,5 @@
+package com.example.antrojiprogramavimopraktika.login
+
+class LoginFragment {
+
+}
---
+++ app/src/main/java/com/example/antrojiprogramavimopraktika/login/LoginViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.antrojiprogramavimopraktika.login
+
+class LoginViewModel {
+
+}
